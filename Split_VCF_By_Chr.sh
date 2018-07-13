655c1a540b51bb1587ed8e9a4537f48c60687844
Alexander Toenges <21218078+ATpoint@users.noreply.github.com>
2018-07-13 17:39:48 +0200

    Create Split_VCF_By_Chr.sh

---
+++ Split_VCF_By_Chr.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+## Assumes at least a sorted VCF, extracts entries by chromosomes using tabix and parallel:
+if [[ ${1: -3} != ".gz" ]]
+  then
+  echo '[INFO]: File does not seem to be compressed -- compressing now'
+  bgzip $1
+  tabix -p vcf ${1}.gz; fi
+
+## Get chromosomes that are present in the VCF:
+tabix ${1}.gz -l > ${1%.vcf.gz}_chr.txt && \
+
+## Extract files by chr:
+cat ${1%.vcf.gz}_chr.txt | parallel "tabix -h ${1}.gz {} > ${1%.vcf.gz}_{}.vcf"
+
+## Usage: ./Split_VCF_By_Chr.sh input.vcf(.gz)
