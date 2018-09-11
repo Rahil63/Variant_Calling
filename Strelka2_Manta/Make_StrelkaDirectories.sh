2f1ccae7881f0e13d8b2a8360a32bda61772f18c
Alexander Toenges <21218078+ATpoint@users.noreply.github.com>
2018-09-11 12:18:17 +0200

    Create Make_StrelkaDirectories.sh

---
+++ Strelka2_Manta/Make_StrelkaDirectories.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+############################################################################################
+############################################################################################
+
+## Assuming BAM files from WGS tumor/normal(*-t/n_SortedRmdup.bam) pairs are in $(pwd) 
+## run the configuration scripts to create folders for variant calling
+
+## See: https://github.com/Illumina/strelka/blob/master/docs/userGuide/README.md
+
+############################################################################################
+############################################################################################
+
+PRIMARY="/scratch/tmp/a_toen03/Genomes/hg38/primary_chromosomes.bed.gz"
+HG38="/scratch/tmp/a_toen03/Genomes/hg38/hg38_noALT_withDecoy.fa"
+STRELKA2="~/software_2c/strelka-2.9.7.centos6_x86_64/bin/"
+
+############################################################################################
+############################################################################################
+
+## Assumes that Manta has already been run to get the candidate indels.
+
+## => Somatic workflow:
+ls *-t*.bam | \
+  awk -F "-t" '{print $1}' | \
+    parallel "$STRELKA2/configureStrelkaSomaticWorkflow.py \
+      --normalBam={}-n_SortedRmdup.bam \
+      --tumorBam={}-t_SortedRmdup.bam \
+      --callRegions=${PRIMARY} \
+      --runDir=Strelka2Dir_{}/Somatic/ \
+      --referenceFasta=${HG38} \
+      --indelCandidates=./MantaDir_{}/Somatic/results/variants/candidateSmallIndels.vcf.gz"
+   
+## => Germline workflow:
+ls *-n* | \
+  awk -F "-n" '{print $1}' | \
+    parallel "$STRELKA2/configureStrelkaGermlineWorkflow.py \
+      --bam={}-n_SortedRmdup.bam \
+      --callRegions=${PRIMARY} \
+      --runDir=Strelka2Dir_{}/Germline \
+      --referenceFasta=${HG38} \
+      --indelCandidates=./MantaDir_{}/Germline/results/variants/candidateSmallIndels.vcf.gz"
+                        
+############################################################################################
+############################################################################################
