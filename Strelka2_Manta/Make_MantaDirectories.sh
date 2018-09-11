9a1ea061b7a5df5aba470bccd7c8dc6a0bee3c51
Alexander Toenges <21218078+ATpoint@users.noreply.github.com>
2018-09-11 12:18:42 +0200

    Create Make_MantaDirectories.sh

---
+++ Strelka2_Manta/Make_MantaDirectories.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+############################################################################################
+############################################################################################
+
+## Assuming BAM files from WGS tumor/normal(*-t/n_SortedRmdup.bam) pairs are in $(pwd) 
+## run the configuration scripts to create folders for SV calling
+
+############################################################################################
+############################################################################################
+
+PRIMARY="/scratch/tmp/a_toen03/Genomes/hg38/primary_chromosomes.bed.gz"
+MANTA="/home/a/a_toen03/software_2c/manta-1.4.0.centos6_x86_64/bin/configManta.py"
+HG38="/scratch/tmp/a_toen03/Genomes/hg38/hg38_noALT_withDecoy.fa"
+
+############################################################################################
+############################################################################################
+
+ls *-t*.bam | \
+  awk -F "-t" '{print $1}' | \
+  parallel "$MANTA \
+    --normalBam={}-n_SortedRmdup.bam \
+    --tumorBam={}-t_SortedRmdup.bam \
+    --callRegions=${PRIMARY} \
+    --runDir=MantaDir_{}/Somatic/ \
+    --referenceFasta=${HG38}"
+    
+ls *-n_SortedRmdup.bam | \
+  awk -F "-n_SortedRmdup.bam" '{print $1}' | \
+  parallel "$MANTA \
+    --normalBam={}-n_SortedRmdup.bam \
+    --callRegions=${PRIMARY} \
+    --runDir=MantaDir_{}/Germline \
+    --referenceFasta=${HG38}"
+    
+############################################################################################
+############################################################################################
