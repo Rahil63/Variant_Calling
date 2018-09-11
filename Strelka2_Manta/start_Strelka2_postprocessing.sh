63c142dcba9bed6dd75e552846c2dde60a32b420
Alexander Toenges <21218078+ATpoint@users.noreply.github.com>
2018-09-11 14:55:00 +0200

    Update start_Strelka2_postprocessing.sh

--- Strelka2_Manta/start_Strelka2_postprocessing.sh
+++ Strelka2_Manta/start_Strelka2_postprocessing.sh
@@ -9,3 +9,13 @@ ls Strelka2Dir_RG* | grep ':' | awk -F ":" '{print $1}' | \
     "cp Strelka2_postprocessing.sh ./{}/Somatic/results/variants && \
      cd ./{}/Somatic/results/variants && \
      ./Strelka2_postprocessing.sh 2> stderr.log"
+
+## Make a new folder with the final variants as symbolic links:
+mkdir Final_Variants
+
+while read p
+  do
+  ln -s \
+    $(realpath ./$p/Somatic/results/variants/somatic.PASSED.ExLC.Ex1KG.vcf.gz) \
+    ./Final_Variants/${p#Strelka2Dir_}_somatic.PASSED.ExLC.Ex1KG.vcf.gz
+  done < <(ls Strelka2Dir_RG* | grep ':' | awk -F ":" '{print $1}')
