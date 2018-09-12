bd8631114cd277f734538af6109d09604e619b47
Alexander Toenges <21218078+ATpoint@users.noreply.github.com>
2018-09-12 14:08:33 +0200

    Create Make-Canvas_Scripts.sh

---
+++ Canvas/Make-Canvas_Scripts.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+## Script produces executable scripts for each BAM pair in $(pwd) to be run with Canvas,
+## submitted to SLURM:
+
+for i in *-t_*.bam; do 
+  BASENAME=${i%-t_SortedRmdup.bam}
+  cat slurm_header.txt \
+    <(echo '#SBATCH --job-name=Canvas_'${BASENAME}) \
+    <(echo '#SBATCH --output='${BASENAME}'_canvas.log') \
+    <(echo 'BASENAME='${BASENAME}) \
+    Canvas_master.sh \
+    > Canvas_run_${BASENAME}.sh; done
+    
+  chmod +x Canvas_run_*.sh  
