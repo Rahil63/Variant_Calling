054848a04eaee064c05b64fa605592e4f868f46d
Alexander Toenges <21218078+ATpoint@users.noreply.github.com>
2018-09-13 20:53:13 +0200

    Update Canvas_master.sh

--- Canvas/Canvas_master.sh
+++ Canvas/Canvas_master.sh
@@ -2,17 +2,16 @@
 
 ## Run Canvas on both tumor and normal WGS samples, assuming strelka germline calls have already been produced.
 ## Note, requires to be run via Make-Canvas_Scripts.sh
-
+## dotnet must be in path!
 ################################################################################################################
 ################################################################################################################
 
-DOTNET="/home/a/a_toen03/software_2c/dotnet"
 CANVAS="/home/a/a_toen03/software_2c/Canvas-1.38.0.1554+master_x64/Canvas.dll"
 #BASENAME=see Make-Scripts_Canvas.sh
 ################################################################################################################
 
 KMER="/scratch/tmp/a_toen03/Genomes/hg38/Canvas/kmer.fa"
-FILTER13="/scratch/tmp/a_toen03/Genomes/hg38/Canvas/filter13.bed"
+FILTER13="/scratch/tmp/a_toen03/Genomes/hg38/Canvas/customExclusion.bed"
 GENOME_FOLDER="/scratch/tmp/a_toen03/Genomes/hg38/Canvas"
 
 ################################################################################################################
@@ -20,9 +19,9 @@ GENOME_FOLDER="/scratch/tmp/a_toen03/Genomes/hg38/Canvas"
 
 #### Somatic workflow:
 echo '[MAIN] Running somatic workflow -- time:' && date
-$DOTNET $CANVAS Somatic-WGS \
+dotnet $CANVAS Somatic-WGS \
   --bam=${BASENAME}-t_SortedRmdup.bam \
-  --somatic-vcf=VariantDir_${BASENAME}/Srelka2/Somatic/result/results/variants/somatic.snvs.vcf.gz \
+  --somatic-vcf=VariantDir_${BASENAME}/Strelka2/Somatic/results/variants/somatic.snvs.vcf.gz \
   --sample-b-allele-vcf=VariantDir_${BASENAME}/Strelka2/Germline/results/variants/variants.vcf.gz \
   --sample-name=${BASENAME}-t \
   --output=VariantDir_${BASENAME}/Canvas/Somatic \
