aa53656a9b0536a700cb187342e63abc74c4d5c9
Alexander Toenges <21218078+ATpoint@users.noreply.github.com>
2018-09-13 15:26:09 +0200

    unified basedir

--- Strelka2_Manta/Make_StrelkaDirectories.sh
+++ Strelka2_Manta/Make_StrelkaDirectories.sh
@@ -27,9 +27,9 @@ ls *-t*.bam | \
       --normalBam={}-n_SortedRmdup.bam \
       --tumorBam={}-t_SortedRmdup.bam \
       --callRegions=${PRIMARY} \
-      --runDir=Strelka2Dir_{}/Somatic/ \
+      --runDir=VariantDir_${BASENAME}/Strelka2/Somatic \
       --referenceFasta=${HG38} \
-      --indelCandidates=./MantaDir_{}/Somatic/results/variants/candidateSmallIndels.vcf.gz"
+      --indelCandidates=VariantDir_${BASENAME}/Manta/Somatic/results/variants/candidateSmallIndels.vcf.gz"
    
 ## => Germline workflow:
 ls *-n* | \
@@ -37,9 +37,9 @@ ls *-n* | \
     parallel "$STRELKA2/configureStrelkaGermlineWorkflow.py \
       --bam={}-n_SortedRmdup.bam \
       --callRegions=${PRIMARY} \
-      --runDir=Strelka2Dir_{}/Germline \
+      --runDir=VariantDir_${BASENAME}/Strelka2/Germline/ \
       --referenceFasta=${HG38} \
-      --indelCandidates=./MantaDir_{}/Germline/results/variants/candidateSmallIndels.vcf.gz"
+      --indelCandidates=VariantDir_${BASENAME}/Manta/Germline/results/variants/candidateSmallIndels.vcf.gz"
                         
 ############################################################################################
 ############################################################################################
